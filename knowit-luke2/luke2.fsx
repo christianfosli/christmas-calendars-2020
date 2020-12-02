fbe49091531e4d00ab8be73ff851064415b97a67
Christian Fosli <christian.fosli@capgemini.com>
2020-12-02 21:26:27 +0100

    Knowit luke 2 -> Improve performance by a lot

--- knowit-luke2/luke2.fsx
+++ knowit-luke2/luke2.fsx
@@ -1,31 +1,32 @@
 let isPrime n =
+    // stolen from https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/sequences#examples
     let rec check i =
         i > n / 2 || (n % i <> 0 && check (i + 1))
 
     check 2
 
-let rec getGoodPresents (presents: seq<int>) =
+let rec getGoodPresents (presents: int list) =
     let good =
         presents
-        |> Seq.takeWhile (fun n -> not (Seq.contains '7' (string n)))
-        |> List.ofSeq
+        |> List.takeWhile (fun n -> not (Seq.contains '7' (string n)))
 
-    let rest = presents |> Seq.skip (Seq.length good)
+    let bad, rest =
+        presents
+        |> List.skip (List.length good)
+        |> fun lst -> (List.head lst, List.skip 1 lst)
 
     let nearestPrime =
-        [ 0 .. (Seq.head rest) ]
-        |> Seq.rev
-        |> Seq.find isPrime
+        [ 0 .. bad ] |> List.rev |> List.find isPrime
 
-    if nearestPrime < Seq.length rest then
+    if nearestPrime < List.length rest then
         good
-        @ getGoodPresents (rest |> Seq.skip 1 |> Seq.skip nearestPrime)
+        @ getGoodPresents (rest |> List.skip nearestPrime)
     else
         good
 
 [ 0 .. (5_433_000 - 1) ]
 |> getGoodPresents
-|> Seq.length
+|> List.length
 |> printfn "%A"
 
 // Tests
@@ -34,5 +35,5 @@ match ([ 0 .. 9 ] |> getGoodPresents) with
 | wrong -> failwithf "got %A" wrong
 
 match ([ 0 .. 19 ] |> getGoodPresents) with
-| [0; 1; 2; 3; 4; 5; 6; 15; 16 ] -> ()
+| [ 0; 1; 2; 3; 4; 5; 6; 15; 16 ] -> ()
 | wrong -> failwithf "got %A" wrong
