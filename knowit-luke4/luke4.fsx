4aeeb2981d250bda1b4277cdba99b7c54e2c60b9
Christian Fosli <christian.fosli@capgemini.com>
2020-12-05 00:32:04 +0100

    Simplify a bit by summing ingredients before baking cakes

--- knowit-luke4/luke4.fsx
+++ knowit-luke4/luke4.fsx
@@ -39,16 +39,10 @@ File.ReadLines "leveringsliste.txt"
           Egg =
               Map.tryFind "egg" ingredients
               |> Option.defaultValue 0 })
-|> Seq.fold (fun (cakes, remIngr) el ->
-    bakeCakes
-        cakes
-        { Sugar = remIngr.Sugar + el.Sugar
-          Flour = remIngr.Flour + el.Flour
-          Milk = remIngr.Milk + el.Milk
-          Egg = remIngr.Milk + el.Milk })
-       (0,
-        { Sugar = 0
-          Flour = 0
-          Milk = 0
-          Egg = 0 })
+|> Seq.reduce (fun acc el ->
+    { Sugar = acc.Sugar + el.Sugar
+      Flour = acc.Flour + el.Flour
+      Milk = acc.Milk + el.Milk
+      Egg = acc.Egg + el.Egg })
+|> fun ingredients -> bakeCakes 0 ingredients
 |> printfn "%A"
