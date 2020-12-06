d30cac677c813dad5f77cff29ebb30f630ebdf40
Christian Fosli <christian.fosli@capgemini.com>
2020-12-06 13:20:19 +0100

    AoC day 6

---
+++ aoc-day6/day6.fsx
@@ -0,0 +1,33 @@
+open System.IO
+
+// customs forms
+// -> questions are represented by single letters
+// -> groups of people are separated by blank lines
+// -> persons within a group are separated by newlines
+
+let customDeclarationsByGroup =
+    File.ReadAllText "puzzle.txt"
+    |> fun text -> text.Trim().Split "\n\n"
+
+// part 1
+let countQuestionsAnswered (answers: string) =
+    answers.Replace("\n", "")
+    |> Set.ofSeq
+    |> Seq.length
+
+customDeclarationsByGroup
+|> Array.map countQuestionsAnswered
+|> Array.sum
+|> printfn "Sum of 'yes'-questions answered by anyone per group: %i"
+
+// part 2
+let countQuestionsAnsweredByEveryone (answers: string) =
+    answers.Split "\n"
+    |> Seq.map Set.ofSeq
+    |> Seq.reduce Set.intersect
+    |> Seq.length
+
+customDeclarationsByGroup
+|> Array.map countQuestionsAnsweredByEveryone
+|> Array.sum
+|> printfn "Sum of 'yes'-questions answered by everyone per group: %i"
