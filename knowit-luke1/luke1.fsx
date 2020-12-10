b845c6613937bd37cf4c7a35f9a97c376751b378
Christian Fosli <christian.fosli@capgemini.com>
2020-12-10 20:13:47 +0100

    Fix CI

--- knowit-luke1/luke1.fsx
+++ knowit-luke1/luke1.fsx
@@ -4,10 +4,19 @@ open FSharp.Data
 // The file contains numbers 1-100_000 in a random order, except for one number.
 // Find the missing number
 
-Http.RequestString
-    "https://julekalender-backend.knowit.no/challenges/1/attachments/numbers.txt"
-|> fun numberString -> numberString.Split ","
-|> Seq.map int
-|> Set.ofSeq
-|> Set.difference (Set.ofSeq [ 1 .. 100_000 ])
+let tryFetchNumbers =
+    try
+        Http.RequestString "https://julekalender-backend.knowit.no/challenges/1/attachments/numbers.txt"
+        |> Some
+    with ex ->
+        printfn "Fetch error: %A" ex
+        None
+
+tryFetchNumbers
+|> Option.bind (fun numberstring ->
+    numberstring.Split ","
+    |> Seq.map int
+    |> Set.ofSeq
+    |> Set.difference (Set.ofSeq [ 1 .. 100_000 ])
+    |> Some)
 |> printfn "%A"
