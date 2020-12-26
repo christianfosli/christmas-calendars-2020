8558d75439fefec6e210fd30b88c972181220bb8
Christian Fosli <christian.fosli@capgemini.com>
2020-12-27 00:39:02 +0100

    Add cache -> completes day 25
    Now the loop_size can be calculated in O(n) rather than O(n^2),
    meaning the it takes ~3 seconds to crack the encryption key, rather than
    _forever_.
    
    Not really happy about the mutable cache, but ¯\_(ツ)_/¯

--- aoc-day25/src/main.rs
+++ aoc-day25/src/main.rs
@@ -1,21 +1,38 @@
-// Used to generate pub key
-fn transform_sub_num(sub: usize, loop_size: usize) -> usize {
-    (0..loop_size).fold(1, |acc, _| (acc * sub) % 20201227)
+use std::collections::HashMap;
+
+const DIVISOR: usize = 20201227;
+
+/// Generates (usually public) key.
+/// Speedy when cache includes computation for `loop_size-1`,
+/// otherwise performs `loop_size` iterations
+fn transform_sub_num(cache: &mut HashMap<usize, usize>, sub: usize, loop_size: usize) -> usize {
+    let transformed = cache
+        .get(&(loop_size - 1))
+        .and_then(|prev| Some((prev * sub) % DIVISOR))
+        .or_else(|| Some((0..loop_size).fold(1, |acc, _| (acc * sub) % DIVISOR)))
+        .unwrap();
+
+    cache.insert(loop_size, transformed);
+
+    transformed
 }
 
 fn find_loop_size(sub: usize, pub_key: usize) -> Option<usize> {
-    (1..).find(|loop_size| transform_sub_num(sub, *loop_size) == pub_key)
+    let mut cache = HashMap::new();
+    (1..).find(|loop_size| transform_sub_num(&mut cache, sub, *loop_size) == pub_key)
 }
 
 fn find_encryption_key(my_loop_size: usize, other_pub_key: usize) -> usize {
-    transform_sub_num(other_pub_key, my_loop_size)
+    let mut cache = HashMap::new();
+    transform_sub_num(&mut cache, other_pub_key, my_loop_size)
 }
 
 fn main() {
     let card_pub_key = 10943862;
     let door_pub_key = 12721030;
+    let subject_number = 7;
 
-    let encryption_key = find_loop_size(7, card_pub_key)
+    let encryption_key = find_loop_size(subject_number, card_pub_key)
         .map(|loop_size| find_encryption_key(loop_size, door_pub_key));
 
     println!("{:?}", encryption_key);
